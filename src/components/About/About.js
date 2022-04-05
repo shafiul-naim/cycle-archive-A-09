674ca39cfd3dc01ab92c077df1964b252559bcab
shafiul-naim <shafiul.naim@gmail.com>
2022-04-05 16:27:54 +0600

    about  added

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='text-3xl text-center p-5 m-5 text-stone-900'>
+            This is about page
+        </div>
+    );
+};
+
+export default About;
